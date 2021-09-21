72bd2597b6233854e6f538cd8b1ebe947c6c5cd7
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2021-09-21 04:25:29 +0300

    add destructor

--- pages/db_config.php
+++ pages/db_config.php
@@ -1,18 +1,23 @@
+<php?
 class ScoreTable {
     const DB_HOST = 'localhost';
     const DB_NAME = 'OsuScoreTable';
     const DB_USER = 'root';
     const DB_PASSWORD = 'root';
-    
+
     private $pdo = null;
 
     public function __construct(){
-        $conStr = sprintf("mysql:host=%s; dbname=%s",self::DB_HOST, self::DB::NAME);
-        try{
-            $this->pdo = new PDO($conStr, self::DB_USER,self::DB_PASSWORD);
-        } catch (PDOException $e){
-            echo($e->getMessage());
-        }
+        $conStr = sprintf("mysql:host=%s; dbname=%s",self::DB_HOST, self::DB_NAME);
+            try{
+                $this->pdo = new PDO($conStr, self::DB_USER,self::DB_PASSWORD);
+            } catch (PDOException $e){
+                echo($e->getMessage());
+            }
         }
     }
+    public function __destruct(){
+        $this->pdo = null;
+    }
 }
+?>
