0a07f32e0716e3a305ee5bde8c76a80fedade19e
SoraHimmel2 <AmelkinAndreiU@yandex.ru>
2021-10-08 02:44:33 +0300

    add feature:split place and mods

--- pages/osu_parser.php
+++ pages/osu_parser.php
@@ -1,35 +1,37 @@
 <?php
-require_once $_SERVER['DOCUMENT_ROOT'] . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
-use Facebook\WebDriver\Chrome\ChromeOptions;
 use Facebook\WebDriver\Remote\DesiredCapabilities;
 use Facebook\WebDriver\Remote\RemoteWebDriver;
 use Facebook\WebDriver\WebDriverBy;
 use Facebook\WebDriver\WebDriverExpectedCondition;
 
+include 'top_score_database.php';
+
 class Parser
 {
-    private $scoreTable = [];
+    private $beatmapScoreboardTable = [];
     private $driver;
+    private $osuScoreDatabase;
 
     public function __construct($mapUrl)
     {
-        $chromeOptions = new ChromeOptions();
-        $chromeOptions->addArguments(['--headless']);
+        //$chromeOptions = new ChromeOptions();
+        //$chromeOptions->addArguments(['--headless']);
 
-        $capabilities = DesiredCapabilities::chrome();
-        $capabilities->setCapability(ChromeOptions::CAPABILITY_W3C, $chromeOptions);
+        //$capabilities = DesiredCapabilities::chrome();
+        //$capabilities->setCapability(ChromeOptions::CAPABILITY_W3C,$chromeOptions);
 
         $serverUrl = 'http://localhost:4444/wd/hub';
 
-        $this->$driver = RemoteWebDriver::create($serverUrl, $capabilities);
+        $this->$driver = RemoteWebDriver::create($serverUrl, DesiredCapabilities::chrome());
 
         $this->$driver->get($mapUrl);
     }
 
     public function getTableData()
     {
-        $listOfScore = $this->$driver->wait(10)->until(
+        $listOfScore = $this->$driver->wait(8)->until(
             WebDriverExpectedCondition::presenceOfAllElementsLocatedBy(WebDriverBy::xpath('//a[@class = "beatmap-scoreboard-table__cell-content beatmap-scoreboard-table__cell-content--score"  ]'))
         );
 
@@ -39,7 +41,7 @@ class Parser
         $listOfMaxCombo = $this->$driver->findElements(WebDriverBy::xpath('//child::td[7]/a'));
         $listOfPp = $this->$driver->findElements(WebDriverBy::xpath('//td/a/span'));
         $listOfTime = $this->$driver->findElements(WebDriverBy::xpath('//td/a/time[@class="js-tooltip-time"]'));
-        $listOfMods = $this->$driver->findElements(WebDriverBy::xpath('
+        $placeAndListOfMods = $this->$driver->findElements(WebDriverBy::xpath('
        //td/a/div/div[@class="mod mod--HD"] |
        //td/a/div/div[@class="mod mod--HR"] |
        //td/a/div/div[@class="mod mod--DT"] |
@@ -49,56 +51,97 @@ class Parser
        //td/a/div/div[@class="mod mod--HT"] |
        //a[@class="beatmap-scoreboard-table__cell-content beatmap-scoreboard-table__cell-content--rank"]'));
 
-        $this->$scoreTable = [
-            'score' => $listOfScore,
-            'accuracy' => $listOfAccuracy,
-            'playerName' => $listOfPlayerName,
-            'maxCombo' => $listOfMaxCombo,
-            'pp' => $listOfPp,
-            'time' => $listOfTime,
-            'mods' => $listOfMods,
-        ];
-    }
+        //$this->$scoreTable = [
+        //    'score' => $listOfScore,
+        //    'accuracy' => $listOfAccuracy,
+        //    'player_name' => $listOfPlayerName,
+        //    'max_combo' => $listOfMaxCombo,
+        //    'pp' => $listOfPp,
+        //    'time' => $listOfTime,
+        //    'mods' => $listOfMods,
+        //];
 
-    public function sendDataToDatabase()
-    {
-        foreach ($this->$scoreTable as $tableCols => $col) {
-            if ($tableCols == 'mods') {
-                foreach ($col as $rowList => $rowData) {
+        /*foreach ($this->$scoreTable as $tableCols => $col) {
 
-                    echo ($rowData->getText() . $rowData->getAttribute('title') . $rowData->getAttribute('data-orig-title') . '<br/>');
+        if ($tableCols == 'mods') {
+        foreach ($col as $rowData) {
 
-                }
-            } elseif ($tableCols == 'maxCombo') {
-                foreach ($col as $rowList => $rowData) {
-                    if ($rowData->getAttribute('class') == 'beatmap-scoreboard-table__cell-content beatmap-scoreboard-table__cell-content--perfect') {
-                        print_r($rowData->getText() . " perfectCombo " . "<br/>");
-                    } else {
-                        print_r($rowData->getText() . '<br/>');
-                    }
+        //echo ($rowData->getText() . $rowData->getAttribute('title') . $rowData->getAttribute('data-orig-title') . '<br/>');
+        $sliceOfData['place'] = $rowData->getText();
+        $sliceOfData[$tableCols] = $rowData->getAttribute('title') . $rowData->getAttribute('data-orig-title');
+        //$this->osuScoreDatabase->insertRow('place', $rowData->getText());
+        //$this->osuScoreDatabase->insertRow('mods', $rowData->getAttribute('title') . $rowData->getAttribute('data-orig-title'));
 
-                }
-            } else {
-                foreach ($col as $rowList => $rowData) {
-                    echo ($rowData->getText() . '<br/>');
-                }
-
-            }
-
-            echo ('<br/>');
+        }
+        } elseif ($tableCols == 'maxCombo') {
+        foreach ($col as $rowData) {
+        if ($rowData->getAttribute('class') == 'beatmap-scoreboard-table__cell-content beatmap-scoreboard-table__cell-content--perfect') {
+        //print_r($rowData->getText() . " perfectCombo " . "<br/>");
+        $sliceOfData[$tableCols] =  $rowData->getText() . 'perfectCombo';
+        } else {
+        $sliceOfData [$tableCols] =  $rowData->getText();
         }
 
+        }
+        } else {
+        foreach ($col as $rowData) {
+        //echo ($rowData->getText() . '<br/>');
+
+        }
+
+        }
+
+        echo ('<br/>');
+        }*/
+        $mods = [];
+        $results = [];
+        $currentState = [];
+        foreach ($placeAndListOfMods as $data) {
+            // print_r($data->getText() . '<br/>');
+            // print_r($data->getAttribute('title') . $data->getAttribute('data-orig-title') . '<br/>');
+
+        }
+        for ($counter = 0; $counter < sizeof($placeAndListOfMods); $counter++) {
+
+            if (!isset($placeAndListOfMods[$counter])) {
+                $currentState[] = $placeAndListOfMods[$counter]->getAttribute('title') . $placeAndListOfMods[$counter]->getAttribute('data-orig-title') .
+                $placeAndListOfMods[$counter]->getText();
+                break;
+            }
+            if (ctype_digit($placeAndListOfMods[$counter]->getText()[1])) {
+                $currentState[] = $placeAndListOfMods[$counter]->getAttribute('title') . $placeAndListOfMods[$counter]->getAttribute('data-orig-title') .
+                $placeAndListOfMods[$counter]->getText();
+                $results[] = $placeAndListOfMods[$counter]->getText();
+            }
+            if (!ctype_digit($placeAndListOfMods[$counter]->getText()[1])) {
+                if (!isset($placeAndListOfMods[$counter + 1])) {
+                    $isReady = true;
+                } else if (ctype_digit($placeAndListOfMods[$counter + 1]->getText()[1])) {
+                    $isReady = true;
+                }
+                $modResult .= $placeAndListOfMods[$counter]->getAttribute('title') . $placeAndListOfMods[$counter]->getAttribute('data-orig-title');
+                $currentState[] = $placeAndListOfMods[$counter]->getAttribute('title') .
+                $placeAndListOfMods[$counter]->getAttribute('data-orig-title') .
+                $placeAndListOfMods[$counter]->getText();
+
+            }
+            if ($isReady) {
+                $mods[] = $modResult;
+                $modResult = " ";
+                $isReady = false;
+            }
+        }
+
+        print_r($mods);
+        echo "<br/>";
+        print_r($results);
+
+    }
+    public function __destruct()
+    {
         $this->$driver->quit();
     }
 
 }
-//$osuParser = new Parser('https://osu.ppy.sh/beatmapsets/1313256#osu/2721646');
-//$osuParser->getTableData();
-//$osuParser->sendDataToDatabase();
-
-include_once 'score_table_db.php';
-$testDb = new ScoreTable();
-$testDb->insertRow('pp','832');
-$testDb->insertRow('pp','988');
-$testDb->showData();
-
+$osuParser = new Parser('https://osu.ppy.sh/beatmapsets/441668#osu/949948');
+$osuParser->getTableData();
